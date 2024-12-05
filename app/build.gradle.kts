db603dd127dd61bf59bfd63f2ac7dcd51fa61410
adrian <aruiz.official@icloud.com>
2024-12-05 13:28:58 -0500

    Changed Version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
     defaultConfig {
         applicationId = "com.anxietystressselfmanagement"
         minSdk = 34
-        versionCode = 2
-        versionName = "1.0"
+        versionCode = 4
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
