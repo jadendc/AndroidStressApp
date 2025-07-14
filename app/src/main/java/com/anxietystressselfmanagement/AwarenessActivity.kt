660c18cd215bef9d978b3ed03c6d09b7e6b7b10f
brian <brianrufin@gmail.com>
2025-07-14 12:24:37 -0400

    Updated

--- app/src/main/java/com/anxietystressselfmanagement/AwarenessActivity.kt
+++ app/src/main/java/com/anxietystressselfmanagement/AwarenessActivity.kt
@@ -6,9 +6,7 @@ import android.os.Bundle
 import android.widget.*
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.viewmodel.compose.viewModel
 import com.anxietystressselfmanagement.ui.activities.StrategiesActionsActivity
-import com.anxietystressselfmanagement.viewmodel.AwarenessViewModel
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import java.text.SimpleDateFormat
@@ -130,7 +128,7 @@ class AwarenessActivity : AppCompatActivity() {
     }
 
     /**
-     * Navigates to the StrategiesActionsActivity.
+     * Navigates to the StrategiesAndActionsActivity.
      */
     private fun navigateToStrategies() {
         val intent = Intent(this, StrategiesActionsActivity::class.java)
