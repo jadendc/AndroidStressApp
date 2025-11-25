9c4472c70eb20524706bac96bee4aa4cd0976dcf
Santiago Boscan <70293375+SantiagoBoscan07@users.noreply.github.com>
2025-11-25 08:51:22 -0500

    release fixes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         applicationId = "com.anxietystressselfmanagement"
         minSdk = 26
         targetSdk = 35
-        versionCode = 22
+        versionCode = 23
         versionName = "2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
